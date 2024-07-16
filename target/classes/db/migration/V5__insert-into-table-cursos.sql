560ea18e316d460b8c82e59fd53a05e2b05acd05
Roberto Ruvalcaba <robertu131@gmail.com>
2024-07-15 22:54:31 -0600

    guardando info

---
+++ target/classes/db/migration/V5__insert-into-table-cursos.sql
@@ -0,0 +1,12 @@
+DELETE FROM `forohub`.`cursos`;
+
+INSERT INTO `forohub`.`cursos` (`id`, `nombre`, `categoria`)
+VALUES ('1', 'Aprendiendo backend', 'BACKEND');
+INSERT INTO `forohub`.`cursos` (`id`, `nombre`, `categoria`)
+VALUES ('2', 'Aprendiendo frontend', 'FRONTEND');
+INSERT INTO `forohub`.`cursos` (`id`, `nombre`, `categoria`)
+VALUES ('3', 'Aprendiendo Java', 'JAVA');
+INSERT INTO `forohub`.`cursos` (`id`, `nombre`, `categoria`)
+VALUES ('4', 'Principios del emprendimiento', 'EMPRENDIMIENTO');
+INSERT INTO `forohub`.`cursos` (`id`, `nombre`, `categoria`)
+VALUES ('5', 'Logica de programacion', 'PROGRAMACION');
---
+++ target/classes/db/migration/V6__insert-into-table-usuarios.sql
@@ -0,0 +1,8 @@
+DELETE FROM `forohub`.`usuarios`;
+
+INSERT INTO `forohub`.`usuarios` (`id`, `nombre`, `correo`, `contrasena`, `rol`)
+VALUES ('1', 'Paco administrador', 'paco.administrador@correo.com', '$2a$10$7X09iFWVJeuAhRxInquTN.ynf9KPv/jXkVxdVaSrod9iZd.57fGsu', 'ADMINISTRADOR');
+INSERT INTO `forohub`.`usuarios` (`id`, `nombre`, `correo`, `contrasena`, `rol`)
+VALUES ('2', 'Pedro moderador', 'pedro.moderador@correo.com', '$2a$10$7X09iFWVJeuAhRxInquTN.ynf9KPv/jXkVxdVaSrod9iZd.57fGsu', 'MODERADOR');
+INSERT INTO `forohub`.`usuarios` (`id`, `nombre`, `correo`, `contrasena`, `rol`)
+VALUES ('3', 'Mario usuario', 'mario.usuario@correo.com', '$2a$10$7X09iFWVJeuAhRxInquTN.ynf9KPv/jXkVxdVaSrod9iZd.57fGsu', 'USUARIO');
